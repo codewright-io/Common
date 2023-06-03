3bd464396c5c51c316fb820c5e023a7463664b4f
Eugene Scully <escully@gmail.com>
2023-06-03 20:54:59 +1000

    Add exception handler

---
+++ Common.Asp/Extensions/IApplicationBuilderExtensions.cs
@@ -0,0 +1,61 @@
+﻿using CodeWright.Common.Asp.Middleware;
+//using CodeWright.Common.Exceptions;
+//using Microsoft.AspNetCore.Diagnostics;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.Extensions.DependencyInjection;
+//using static System.Net.Mime.MediaTypeNames;
+
+namespace Microsoft.AspNetCore.Builder;
+
+public static class IApplicationBuilderExtensions
+{
+    public static IApplicationBuilder UseCodeWrightExceptionHandler(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ExceptionMiddleware>();
+        //app.UseExceptionHandler(exceptionHandlerApp =>
+        //{
+        //    exceptionHandlerApp.Run(async context =>
+        //    {
+        //        if (context.RequestServices.GetService<IProblemDetailsService>() is { } problemDetailsService)
+        //        {
+        //            string title;
+        //            string detail;
+        //            string type;
+
+        //            var exceptionHandlerFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+        //            var exceptionType = exceptionHandlerFeature?.Error;
+        //            if (exceptionType != null && exceptionType is NotFoundException)
+        //            {
+        //                title = "Not Found";
+        //                detail = "Item not found";
+        //                type = "https://errors.example.com/notFound";
+        //                context.Response.StatusCode = StatusCodes.Status404NotFound;
+        //                context.Response.ContentType = Text.Plain;
+        //            }
+        //            else
+        //            {
+        //                title = "Internal Error";
+        //                detail = "Please Report";
+        //                type = "https://errors.example.com/internalError";
+        //                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        //                context.Response.ContentType = Text.Plain;
+        //            }
+
+        //            await problemDetailsService.WriteAsync(new ProblemDetailsContext
+        //            {
+        //                HttpContext = context,
+        //                ProblemDetails =
+        //        {
+        //            Title = title,
+        //            Detail = detail,
+        //            Type = type
+        //        }
+        //            });
+        //        }
+        //    });
+        //});
+
+        return app;
+    }
+}
---
+++ Common.Asp/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using System.Text.Json;
+using CodeWright.Common.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace CodeWright.Common.Asp.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger _logger;
+
+    public ExceptionMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<ExceptionMiddleware>();
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Something went wrong: {ex}");
+            await HandleExceptionAsync(httpContext, ex);
+        }
+    }
+
+    private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+
+        HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+        switch (exception)
+        {
+            case NotFoundException:
+                statusCode = HttpStatusCode.NotFound;
+                break;
+            case BadRequestException:
+                statusCode = HttpStatusCode.BadRequest;
+                break;
+            case InvalidInternalStateException:
+                statusCode = HttpStatusCode.Conflict;
+                break;
+        }
+        context.Response.StatusCode = (int)statusCode;
+
+        var response = new ErrorResponse
+        {
+            Type = exception.GetType().Name,
+            Message = exception.Message,
+            StackTrace = exception.StackTrace ?? "", // TODO: Developer flag
+        };
+
+        var responseString = JsonSerializer.Serialize(response);
+        await context.Response.WriteAsync(responseString);
+    }
+}
