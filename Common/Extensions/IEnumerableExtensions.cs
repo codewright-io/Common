0db0c826dd02b63a09726352171abdc0ff7e8dd6
Eugene Scully <escully@gmail.com>
2023-06-10 11:52:02 +1000

    Add any and not null extension

---
+++ Common/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace System.Linq;
+
+/// <summary>
+/// Extensions for IEnumerable
+/// </summary>
+public static class IEnumerableExtensions
+{
+    /// <summary>
+    /// Return true if the enumerable is not null and contains one or more items.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="items"></param>
+    /// <returns></returns>
+    public static bool AnyAndNotNull<T>(this IEnumerable<T> items) => items != null && items.Any();
+}
