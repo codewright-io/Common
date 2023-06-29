34e6baabb6fcbc392ab06e247d1317cbfe98616c
Eugene Scully <escully@gmail.com>
2023-06-30 00:06:03 +1000

    Add ID to command result

--- Common.EventSourcing/CommandResult.cs
+++ Common.EventSourcing/CommandResult.cs
@@ -12,5 +12,12 @@ public class CommandResult
     /// </summary>
     /// <example>123456</example>
     [Required]
-    public long Version { get; init; }
+    public required long Version { get; init; }
+
+    /// <summary>
+    /// Unique identifier for the object (unique within the tenancy)
+    /// </summary>
+    /// <example>A_Midsummer_Nights_Dream</example>
+    [Required]
+    public required string Id { get; init; }
 }
