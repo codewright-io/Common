4ca09b69a70f3e918bc73e1dcaa8b87d6c491c45
Eugene Scully <escully@gmail.com>
2023-07-02 13:36:21 +1000

    Update rountes

--- Common.Asp/Routes/HttpGet.cs
+++ Common.Asp/Routes/HttpGet.cs
@@ -50,6 +50,29 @@ public static class HttpGet
         => $"api/policy-target/v1/evaluate?targetId={targetId}&targetAction={targetAction}";
     #endregion Policy
 
+    #region Tags
+
+    public static string ItemMetadata(string tenantId, string id) => $"api/items/metadata/v1/{tenantId}/{id}";
+
+    public static string ItemRelationships(string tenantId, string id) => $"api/items/relationships/v1/{tenantId}/{id}";
+
+    public static string Referencing(string tenantId, string targetId)
+        => $"api/items/relationships/v1/referencing/{tenantId}/{targetId}";
+
+    public static string ItemTags(string tenantId, string id) => $"api/items/tags/v1/{tenantId}/{id}";
+
+    public static string ItemsByTag(string tenantId, string tag) => $"api/items/tags/v1/search/{tenantId}?tag={tag}";
+
+    public static string ItemEvents(string tenantId, string id, int limit) => $"api/items/v1/events/{tenantId}/{id}?limit={limit}";
+
+    public static string SearchMetadata(string tenantId, string name, string value, string secondaryName, string secondaryValue, int limit, int offset)
+        => $"api/items/metadata/v1/search/{tenantId}?name={name}&value={value}&secondaryName={secondaryName}&secondaryValue={secondaryValue}&limit={limit}&offset={offset}";
+
+    public static string SearchMetadata(string tenantId, string name, string value, int limit, int offset)
+        => $"api/items/metadata/v1/search/{tenantId}?name={name}&value={value}&limit={limit}&offset={offset}";
+
+    #endregion Tags
+
     /// <summary>
     /// Fetch the swagger index.html
     /// </summary>
--- Common.Asp/Routes/HttpPost.cs
+++ Common.Asp/Routes/HttpPost.cs
@@ -30,4 +30,25 @@ public static class HttpPost
     /// </summary>
     public static string PolicyTargetRemove => $"api/policy-target/v1/remove";
     #endregion Policy
+
+    #region Tags
+    public static string AddMetadata() => "api/items/metadata/v1/add";
+
+    public static string RemoveMetadata() => "api/items/metadata/v1/remove";
+
+    public static string SetMetadata() => "api/items/metadata/v1";
+
+    public static string AddRelationships() => "api/items/relationships/v1/add";
+
+    public static string RemoveRelationships() => "api/items/relationships/v1/remove";
+
+    public static string SetReflationships() => "api/items/relationships/v1";
+
+    public static string AddTags() => "api/items/tags/v1/add";
+
+    public static string RemoveTags() => "api/items/tags/v1/remove";
+
+    public static string SetTags() => "api/items/tags/v1";
+
+    #endregion Tags
 }
