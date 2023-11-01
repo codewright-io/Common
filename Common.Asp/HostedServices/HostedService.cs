d8f7b2143de3cffa2500746cf66a339bac0d56f1
Eugene Scully <escully@gmail.com>
2023-11-01 18:56:55 +1100

    Add a periodic hosted service

---
+++ Common.Asp/HostedServices/HostedService.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace CodeWright.Common.Asp.HostedServices;
+
+/// <summary>
+/// Details here : https://www.stevejgordon.co.uk/asp-net-core-2-ihostedservice
+/// 
+/// This code provided by David Fowler: https://gist.github.com/davidfowl/a7dd5064d9dcf35b6eae1a7953d615e3
+/// </summary>
+public abstract class HostedService : IHostedService
+{
+    private Task? _executingTask = null;
+    private CancellationTokenSource? _cts = null;
+
+    /// <inheritdoc />
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        // Create a linked token so we can trigger cancellation outside of this token's cancellation
+        _cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+
+        // Store the task we're executing
+        _executingTask = ExecuteAsync(_cts.Token);
+
+        // If the task is completed then return it, otherwise it's running
+        return _executingTask.IsCompleted ? _executingTask : Task.CompletedTask;
+    }
+
+    /// <inheritdoc />
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        // Stop called without start
+        if (_executingTask == null)
+        {
+            return;
+        }
+
+        // Signal cancellation to the executing method
+        if (_cts is not null)
+            _cts.Cancel();
+
+        // Wait until the task completes or the stop token triggers
+        await Task.WhenAny(_executingTask, Task.Delay(-1, cancellationToken));
+
+        // Run tear-down
+        await TeardownAsync();
+
+        // Throw if cancellation triggered
+        cancellationToken.ThrowIfCancellationRequested();
+    }
+
+    /// <summary>
+    /// Derived classes should override this and execute a long running method until 
+    /// cancellation is requested    
+    /// </summary>
+    protected abstract Task ExecuteAsync(CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Derived classes should optionally override this and execute any key tear down
+    /// logic which may be required for clean termination.
+    /// </summary>
+    protected virtual Task TeardownAsync()
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ Common.Asp/HostedServices/PeriodicHostedService.cs
@@ -0,0 +1,89 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CodeWright.Common.Asp.HostedServices;
+
+/// <summary>
+/// Create a service that executes a function periodically
+/// </summary>
+public class PeriodicHostedService : HostedService, IHostedService
+{
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+    private readonly string _serviceName;
+    private readonly Func<IServiceProvider, CancellationToken, Task<TimeSpan>> _periodicFunc;
+    private readonly Func<IServiceProvider, CancellationToken, Task>? _startFunc;
+    private readonly int _exceptionDelay;
+
+    /// <summary>
+    /// Create an instance of a PeriodicHostedService
+    /// </summary>
+    public PeriodicHostedService(
+        IServiceScopeFactory serviceScopeFactory,
+        string serviceName,
+        Func<IServiceProvider, CancellationToken, Task<TimeSpan>> periodicFunc,
+        Func<IServiceProvider, CancellationToken, Task>? startFunc = null,
+        int exceptionDelay = 10000)
+    {
+        _serviceScopeFactory = serviceScopeFactory;
+        _serviceName = serviceName;
+        _periodicFunc = periodicFunc;
+        _startFunc = startFunc;
+        _exceptionDelay = exceptionDelay;
+    }
+
+    /// <summary>
+    /// Service main loop
+    /// </summary>
+    [SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Hosted Service Loop")]
+    protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        // Run the startup function
+        if (_startFunc is not null && !cancellationToken.IsCancellationRequested)
+        {
+            using var scope = _serviceScopeFactory.CreateScope();
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<PeriodicHostedService>>();
+
+            try
+            {
+                await _startFunc(scope.ServiceProvider, cancellationToken);
+            }
+            catch (OperationCanceledException ex)
+            {
+                logger.LogInformation(ex, "{ServiceName}: Canceled during start", _serviceName);
+            }
+            catch (Exception ex)
+            {
+                // Catch all, we always want to let the hosted service start
+                logger.LogError(ex, "{ServiceName}: Exception during start", _serviceName);
+            }
+        }
+
+        // Continually loop
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            using var scope = _serviceScopeFactory.CreateScope();
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<PeriodicHostedService>>();
+
+            try
+            {
+                // Run the provided function, and wait until the next loop
+                var delay = await _periodicFunc(scope.ServiceProvider, cancellationToken);
+                if (!cancellationToken.IsCancellationRequested)
+                    await Task.Delay(delay, cancellationToken);
+            }
+            catch (OperationCanceledException ex)
+            {
+                logger.LogInformation(ex, "{ServiceName}: Canceled during run", _serviceName);
+                return;
+            }
+            catch (Exception ex)
+            {
+                // Catch all, the show must go on
+                logger.LogError(ex, "{Name} - Uncaught Exception", _serviceName);
+                await Task.Delay(_exceptionDelay, cancellationToken);
+            }
+        }
+    }
+}
